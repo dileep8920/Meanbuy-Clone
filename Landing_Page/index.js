f7696ecdad43bccac146add1fb669adffbb8009f
dileep8920 <dileepkumary669@gmail.com>
2022-08-25 18:02:08 +0530

    static page complete

---
+++ Landing_Page/index.js
@@ -0,0 +1,6 @@
+import navbar from "../component/navbar.js"
+document.getElementById("navbar").innerHTML=navbar()
+
+
+import footer from "../component/footer.js"
+document.getElementById("footer").innerHTML=footer()
---
+++ component/footer.js
@@ -0,0 +1,77 @@
+let footer=()=>{
+    return `
+    <div class="fcontainer">
+    <div>
+        <div class="box">
+            <div class="innerbox">
+                <h2>CORPORATE</h2>
+                <ul>
+                    
+                    
+                    <li><a href="">Sell With Us</a></li>
+                    <li><a href="">Career Opportunities</a></li>            
+                    <li><a href="">Sitemap</a></li>
+                </ul>
+            </div>
+            <div class="innerbox">
+                <h2>OUR POLICIES</h2>
+                <ul>
+                    <li><a href="">Covid-19 Precautions</a></li>
+                    <li><a href="">Our Promise</a></li>
+                    <li><a href="">Terms & Conditions</a></li>
+                    <li><a href="">Privacy Policy</a></li>
+                </ul>
+            </div>
+            <div class="innerbox">
+                <h2>HELP</h2>
+        <ul>
+            <li><a href="">How MeanBuy Works</a>
+            <li><a href="">My Orders</a></li>
+            <li><a href="">Exchanges, Returns and Refunds</a></li>
+            <li><a href="">Contact Us</a></li>
+            <li><a href="">FAQ</a></li>
+        </ul>
+            </div>
+            <div class="innerbox">
+                <h2 class="heading">SELL WITH US</h2>
+                <a href=''>General Information</a><br>
+            <a href=''>Signup</a><br><br>
+            </div>
+            <div class="innerbox" id="lastbox">
+                <input type="email" placeholder="Your Email address" id="emailbox">
+                <button id="sbutton">Subscribe   </button><br>
+                <img src="payment option.png" alt="Image"  id="fimage"><br>
+                <img src="https://static.tildacdn.com/tild3539-6464-4331-b235-313238326462/__photo_2022-03-07_1.jpg" alt="" id="safe">
+            </div>
+            
+        </div>
+    </div>
+    <hr>
+    <div>
+        <div class="row">
+            <h1>Meanbuy Is Your One Stop Destination For Online Shopping</h1><p>Meanbuy is a comprehensive platform for you to choose how you shop, whether it is buying instantly,
+waiting to save or to pick up your order from the store after freezing it.<p><p>Everyone will agree that it is not easy to go down to the market in massive traffic, driving through crowds,
+all striving for the same trendy clothing, expecting in line to try on the picked up items in the trial rooms,
+and finally deciding on the product you buy. Some people like to personally analyze the product before
+purchasing it in the store. Although some people do not like to shop online, others beg to differ by using the
+internet to do things. For all you internet enthusiasts who like online shopping,</p><h2>Get The Best Of Your Online Shopping Experience</h2>
+<p>Keeping mind the fact that everything is just a tap away in today&#39;s world, fashion and clothing have no
+escape from the cyberspace. Online shopping is made easier with various categories and filters that let
+you get the best result of what you&#39;re looking for. At Meanbuy.com we specialize in categorizing the required
+fields as per your convenience to provide you the best online shopping experience. Categories like Online
+shopping for kids, online shopping for men, online shopping for women, online shopping for accessories,
+etc. is curated to make this experience even more convenient. Find the Best Online Shopping in
+Hyderabad only at Meanbuy.</p><h2>Meanbuy Let You choose At Your Convenience</h2><p>What if you belong to the former kind, where you like to buy the product from the shop after analyzing it
+personally? But don&#39;t want to miss out on a fantastic product in the process? We got your back! At
+Meanbuy.com, you can freeze the product and pick up from your neighboring store without the worry of
+missing the product. Another additional feature of this website is, you can wait and save on the products
+you like.</p>		</div>
+    </div>
+        <div id="subfooter">
+            &copy; MeanBuy.com. All Rights Reserved
+        </div>
+ </div>
+    `
+}
+
+export default footer;
