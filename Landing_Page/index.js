ca5d7d9c4fcdf8b11b4ab1ae60b922a15210e60b
dileep8920 <dileepkumary669@gmail.com>
2022-08-28 08:16:34 +0530

    okay

--- Landing_Page/index.js
+++ Landing_Page/index.js
@@ -5,4 +5,4 @@ document.getElementById("navbar").innerHTML=navbar()
 import footer from "../component/footer.js"
 document.getElementById("footer").innerHTML=footer()
 
-let login=localStorage.setItem("login",false)
+// let login=localStorage.setItem("login",false)
